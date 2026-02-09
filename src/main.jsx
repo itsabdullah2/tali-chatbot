0525e025a7df5f084ca4ac31c79e6496b25c41f5
itsabdullah2 <abdullahosman3020@gmail.com>
2026-02-09 10:39:53 +0200

    wrap the App with the context provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import "./index.css";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { StrictMode } from "react";
+import { AppStateProvider } from "./context/AppContext";
 
 const element = document.getElementById("root");
 const root = ReactDOM.createRoot(element);
 
 root.render(
   <StrictMode>
-    <App />
+    <AppStateProvider>
+      <App />
+    </AppStateProvider>
   </StrictMode>,
 );
